1ec4066c85b809d36cfbebd1e58e327f5c13d9d7
Chris D <cdiamond94@Outlook.com>
2022-12-20 03:04:48 -0500

    Pseudocode for next steps

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ let weapon = ["Rock", "Paper", "Scissors"];
 
 const compChoice = getComputerChoice(weapon);
 console.log(compChoice);
+
+//Write a function that plays a single round ex; playRound() = > {};
+//function takes two parameters.
+//playerSelection & computerSelection.
+//RETURNS a string that declares the winner.
+//make playerSelection parameter case-insensitive.
