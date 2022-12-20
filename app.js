9ee60f74dedc3b50d1cdac727f85eee925e81ab0
Chris D <cdiamond94@Outlook.com>
2022-12-20 02:48:17 -0500

    Added computerChoice funtion into app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function getComputerChoice(weapon) {
+    const randIndex = Math.floor(Math.random() * weapon.length);
+    console.log(randIndex);
+    const item = weapon[randIndex];
+    return item;
+}
+
+let weapon = ["Rock", "Paper", "Scissors"];
+
+const compChoice = getComputerChoice(weapon);
+console.log(compChoice);
