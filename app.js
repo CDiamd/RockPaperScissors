22c97b52b04a3f03ecf8d728cf2c1738ad7273a2
Chris D <cdiamond94@Outlook.com>
2022-12-21 23:52:32 -0500

    playRound Function Under Construction.*Debug*

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-function getComputerChoice(arr) {
+function getComputerChoice() {
     const randIndex = Math.floor(Math.random() * weapon.length);
-    console.log(randIndex);
     const item = weapon[randIndex];
     return item;
 }
@@ -10,14 +9,33 @@ let weapon = ["Rock", "Paper", "Scissors"];
 const compChoice = getComputerChoice(weapon);
 console.log(compChoice);
 
-//Write a function that plays a single round ex; playRound() = > {};
-//function takes two parameters.
-//playerSelection & computerSelection.
-//RETURNS a string that declares the winner.
-//make playerSelection parameter case-insensitive.
-//*************T H O U G H T S**************************************
-//Use if-else statements to check comparisons on what item
-//beats what item.... 
-// if player===rock & comp !== rock \\ scissors then
-// comp is winner...
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == "Rock" && computerSelection == "Paper"){
+        return "You Lose! Paper beats Rock!";
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+        return "You Win! Rock beats Scissors!";
+    } else {
+        return "CAT! Try Again!"
+    }
+    if(playerSelection == "Paper" && computerSelection == "Rock"){
+        return "You Win! Paper beats Rock!!";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        return "You Lose! Scissors beats Paper!";
+    } else {
+        return "CAT! Try Again!"
+    }
+    if(playerSelection == "Scissors" && computerSelection == "Rock"){
+        return "You Lose! Rock beats Scissors!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You Win! Scissors beats Paper!";
+    } else {
+        return "CAT! Try Again!"
+    }
+}
+let playerSelection = "Paper";
+let computerSelection = compChoice;
+let results = playRound(playerSelection, computerSelection);
+console.log(results)
+
+
 
