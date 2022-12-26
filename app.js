fe231291ce39d2cf3b3ddb22eb987a8175851fb3
Chris D <cdiamond94@Outlook.com>
2022-12-26 16:22:52 -0500

    CleanerCode (Scorekeepr Under Construction)

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
-
-
 //Grabs random index of weapons array and returns it.
+let weapon = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     const randIndex = Math.floor(Math.random() * weapon.length);
     const item = weapon[randIndex];
     return item;
 }
 
-let weapon = ["Rock", "Paper", "Scissors"];
-
 //performs RPS checks 
-function playRound(playerSelection, computerSelection) {
-    let youWin = `You Win! ${playerSelection} beats ${computerSelection}!`;
-    let youLose = `You Lose! ${computerSelection} beats ${playerSelection}!`;
-    let cat = "CAT! Try Again!";
-        if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
-            (playerSelection == "Paper" && computerSelection == "Rock") ||
-            (playerSelection == "Scissors" && computerSelection == "Paper")) {
-                return youWin;       
-            } else if (playerSelection == computerSelection) {
-                return cat;
-            } else {
-                return youLose;
-            }
-    }
-//Prompts user for input and then sets input as player choice
-let playerSelection = playerInput();
+let youWin = "You Win!";
+let youLose = "You Lose!";
+let cat = "CAT! Try Again!";
 
-function playerInput() {
+let pScore = 0
+let compScore = 0
+console.log(pScore);
+console.log(compScore);
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+            pScore++;
+            return (youWin + ` ${playerSelection} beats ${computerSelection}!`);       
+        } else if (playerSelection == computerSelection) {
+            return (cat);
+        } else {
+            compScore++;
+            return (youLose + ` ${computerSelection} beats ${playerSelection}!`);
+        }
+}
+//Prompts user for input and then sets input as player choice
+function pInput() {
     let pInput = prompt("Rock, Paper, or Scissors?")
     const pUpper = pInput.charAt(0).toUpperCase() + pInput.slice(1);
     return pUpper;
 }
 
 //Runs getCompChoice and RPS checks and prints them 5 times and checks/updates score.
-let playerScore = 0
-let compScore = 0
-
-console.log(playerScore);
-console.log(compScore);
 
 function game() {
     for (let i = 0; i < 5; i++) {
+        let playerSelection = pInput();
         let compChoice = getComputerChoice(weapon);
         let computerSelection = compChoice;
         let result = playRound(playerSelection, computerSelection);
